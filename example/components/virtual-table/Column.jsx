9208a90054c708517851e9606659eaafd9f260c1
Giving Wu <givingwu@gmail.com>
2020-06-24 11:04:02 +0800

    feat: vue-virtualized-table demo page

--- example/components/virtual-table/Column.jsx
+++
@@ -1,8 +0,0 @@
-/* Forked from ant-design-vue 💖 */
-/* eslint-disable vue/require-default-prop */
-import { ColumnProps } from './interface'
-
-export default {
-  name: 'TableColumn',
-  props: ColumnProps
-}
